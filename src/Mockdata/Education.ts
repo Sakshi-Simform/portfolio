d47ad4d2a19b8bcdf388d86547c1fd832f01583a
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-27 12:08:04 +0530

    feature:add mockdata for features

---
+++ src/Mockdata/Education.ts
@@ -0,0 +1,15 @@
+import type { Education } from "../types/type"
+
+export const education: Array<Education> = [
+    
+  {
+   "title": "Bachelor of Science in Information Technology",
+   "institution": "GLS University",
+   "year":"2020-2023"
+  },
+  {
+  "title": "Master of Science in Information Technology",
+  "institution": "GLS University",
+  "year":"2023-2025"
+ },
+]
