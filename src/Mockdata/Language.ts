509ce8be308645c91875af211b2f13bad93cb5f5
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-27 17:38:56 +0530

    feature: add data

---
+++ src/Mockdata/Language.ts
@@ -0,0 +1,12 @@
+import type{ Language } from "../types/type";
+
+export const languages: Language = [
+    'HTML',
+    'CSS',
+    'REACT',
+    'FLUTTER',
+    'MLP',
+    'RSTUDIO',
+    'JAVASCRIPT',
+    'TYPESCRIPT'    
+]as const;
