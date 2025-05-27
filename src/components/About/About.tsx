abc63db35563f0182ab9ef7c31358540bde8ce85
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-27 13:43:40 +0530

    feature: add new div

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,59 +7,61 @@ import cursorIcon from '../../../assets/about/cursorIcon.png'
 import uiIcon from '../../../assets/about/uiIcon.png'
 import serverIcon from '../../../assets/about/serverIcon.png'
 
-export  const About: React.FC = () => {
+export const About: React.FC = () => {
   return (
-    <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
-      <div className={styles.content}>
-        <img
-          src={aboutImage}
-          alt="Me sitting with a laptop"
-          className={styles.aboutImage}
-        />
-        <ul className={styles.aboutItems}>
-          <li className={styles.aboutItem}>
-            <img src={cursorIcon} alt="Cursor icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites.
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={serverIcon} alt="Server icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Education</h3>
-              <div className={styles.educationDetail}>
-                {education.map((education, index) => (
-                  <p key={index}>
-                    <strong className={styles.year}>{education.year}</strong>
-                    <br />
-                    <span>{education.title}</span>
-                    <br />
-                    <span>{education.institution}</span>
-                  </p>
-                ))}
+    <div className={styles.aboutContainer}>
+      <section className={styles.container} id="about">
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+          <img
+            src={aboutImage}
+            alt="Me sitting with a laptop"
+            className={styles.aboutImage}
+          />
+          <ul className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
+              <img src={cursorIcon} alt="Cursor icon" />
+              <div className={styles.aboutItemText}>
+                <h3>Frontend Developer</h3>
+                <p>
+                  I'm a frontend developer with experience in building responsive
+                  and optimized sites.
+                </p>
               </div>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={uiIcon} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Languages</h3>
-              <p className={styles.iconDisplay}>
-                {languages.map((language, index) => (
-                  <button key={index} className={styles.language}>
-                    {language}
-                  </button>
-                ))}
-              </p>
-            </div>
-          </li>
-        </ul>
+            </li>
+            <li className={styles.aboutItem}>
+              <img src={serverIcon} alt="Server icon" />
+              <div className={styles.aboutItemText}>
+                <h3>Education</h3>
+                <div className={styles.educationDetail}>
+                  {education.map((education, index) => (
+                    <p key={index}>
+                      <strong className={styles.year}>{education.year}</strong>
+                      <br />
+                      <span>{education.title}</span>
+                      <br />
+                      <span>{education.institution}</span>
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </li>
+            <li className={styles.aboutItem}>
+              <img src={uiIcon} alt="UI icon" />
+              <div className={styles.aboutItemText}>
+                <h3>Languages</h3>
+                <p className={styles.iconDisplay}>
+                  {languages.map((language, index) => (
+                    <button key={index} className={styles.language}>
+                      {language}
+                    </button>
+                  ))}
+                </p>
+              </div>
+            </li>
+          </ul>
         </div>
-    </section>
+      </section>
+    </div>
   );
 };
