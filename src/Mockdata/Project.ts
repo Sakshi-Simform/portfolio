ead4b930d0074f0dc323f7034c90d26e67c95b6b
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-29 15:34:12 +0530

    feature: change id

--- src/Mockdata/Project.ts
+++ src/Mockdata/Project.ts
@@ -2,7 +2,7 @@ import type { Project } from "../types/type"
 
 export const project: Array<Project> = [
   { 
-    id:crypto.randomUUID(),
+    id:1,
     image: "https://th.bing.com/th/id/OIP.wHFSQ73Wtu_EMXUV578XgwHaFj?rs=1&pid=ImgDetMain",
     title: "Post Management",
     description: "CRUD app that allows users to create, read, update, and delete posts, with an infinite scroll feature..",
@@ -11,7 +11,7 @@ export const project: Array<Project> = [
     source: "https://github.com/Sakshi-Simform/product-management/tree/dev"
   },
   {
-    id:crypto.randomUUID(),
+    id:2,
     image: "https://th.bing.com/th/id/OIP.ccoQc9qPxXON9NZYlRT8qQHaEq?rs=1&pid=ImgDetMain",
     title: "Split Tracker",
     description: "App made for splitting, and track expenses among participants with local storage support.",
@@ -20,7 +20,7 @@ export const project: Array<Project> = [
     source: "https://github.com/dhruvdhaduk-simform/fair-share/tree/dev"
   },
   {
-    id:crypto.randomUUID(),
+    id:3,
     image: "https://th.bing.com/th/id/OIP.a85wrEeztauWYlgOOOD-KgHaHa?cb=iwc2&rs=1&pid=ImgDetMain",
     title: "CookBook",
     description: "Recipe with images and flip-card functionality for detailed views using reusable components..",
