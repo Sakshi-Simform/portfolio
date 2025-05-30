33dfc4f6a8038b38c171d392ab0e5b183ba1097c
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-05-30 13:44:12 +0530

    feature: add validation for form in inline

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,36 +1,12 @@
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
+import { validateEmail ,validateMessage , validateName } from "../../utils/validation";
 import styles from "../../styles/Contact.module.css";
 import "react-toastify/dist/ReactToastify.css";
 import emailIcon from "../../../assets/contact/emailIcon.png";
 import githubIcon from "../../../assets/contact/githubIcon.png";
 import linkedinIcon from "../../../assets/contact/linkedinIcon.png";
-
-function validateName(name: string): string | null {
-  name = name.trim();
-  if (!name) return "Please provide your name";
-  if (name.length > 20) return "Name must not exceed 20 characters";
-  if (!/^[a-zA-Z][a-zA-Z ]*$/.test(name))
-    return "Name must only contain letters and spaces (not starting with a space)";
-  return null;
-}
-
-function validateEmail(email: string): string | null {
-  email = email.trim();
-  if (!email) return "Please provide an email";
-  if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-    return "Invalid Email";
-  }
-  return null;
-}
-
-function validateMessage(message: string): string | null {
-  message = message.trim();
-  if (!message) return "Message is required.";
-  if (message.length > 200) return "Message must not exceed 200 characters";
-  return null;
-}
 
 export const Contact: React.FC = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -103,34 +79,34 @@ export const Contact: React.FC = () => {
       </div>
 
       <form className={styles.form} ref={form} onSubmit={sendEmail} noValidate>
-        <label>Name</label>
-        <input
-          type="text"
-          name="from_name"
-          placeholder="Name"
-          onChange={handleNameChange}
-        />
-        {nameError && <p className={styles.error}>{nameError}</p>}
+  <label>Name</label>
+  <input
+    type="text"
+    name="from_name"
+    placeholder="Name"
+    onChange={(e) => setNameError(validateName(e.target.value))}
+  />
+  {nameError && <p className={styles.error}>{nameError}</p>}
 
-        <label>Email</label>
-        <input
-          type="email"
-          name="from_email"
-          placeholder="Email"
-          onChange={handleEmailChange}
-        />
-        {emailError && <p className={styles.error}>{emailError}</p>}
+  <label>Email</label>
+  <input
+    type="email"
+    name="from_email"
+    placeholder="Email"
+    onChange={(e) => setEmailError(validateEmail(e.target.value))}
+  />
+  {emailError && <p className={styles.error}>{emailError}</p>}
 
-        <label>Message</label>
-        <textarea
-          name="message"
-          placeholder="Message"
-          onChange={handleMessageChange}
-        />
-        {messageError && <p className={styles.error}>{messageError}</p>}
+  <label>Message</label>
+  <textarea
+    name="message"
+    placeholder="Message"
+    onChange={(e) => setMessageError(validateMessage(e.target.value))}
+  />
+  {messageError && <p className={styles.error}>{messageError}</p>}
 
-        <input className={styles.submit} type="submit" value="Send" />
-      </form>
+  <input className={styles.submit} type="submit" value="Send" />
+</form>
 
       <ul className={styles.links}>
         <li className={styles.link}>
